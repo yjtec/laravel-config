392d8698e231e3b27c0dfba28a44115bb9b7060d
kidkang <723616880@qq.com>
2019-04-28 14:57:15 +0800

    read data from database if cache data is empty

--- src/YjConfig.php
+++ src/YjConfig.php
@@ -1,12 +1,23 @@
-<?php 
+<?php
 namespace Yjtec\Config;
-class YjConfig{
+
+class YjConfig
+{
     private $data;
-    public function __construct($data){
+    public function __construct($data)
+    {
+        if (!$data) {
+            $list = \Yjtec\Config\Models\Config::get();
+            $data = $list->mapWithKeys(function ($item) {
+                return [$item['key'] => $item['value']];
+            });
+            \MC::set('yj_config_gloabl', $data->toArray());
+        }
         $this->data = $data;
     }
-    public function get($key){
-        if(isset($this->data[$key])){
+    public function get($key)
+    {
+        if (isset($this->data[$key])) {
             return $this->data[$key];
         }
         return '';
